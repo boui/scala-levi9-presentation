6396065534d8318e2406720c58f5c5b59634aa18
boui <rocksyseletska2837@gmail.com>
2014-08-28 17:34:02 +0300

    adding channels

---
+++ img/promises-catch,png_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"id":"http:\/\/www.html5rocks.com\/tutorials\/es6\/promises\/","comments":138}]});
+}
